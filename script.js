fdce6dbaad09044ee3cc1c74870a001a13da95e8
SheltonT200 <155662867+SheltonT200@users.noreply.github.com>
2024-03-26 11:07:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -207,10 +207,10 @@ function supportRenderTextureFormat (gl, internalFormat, format, type) {
 
 //function startGUI () {
     var gui = new lil.GUI({ width: 300 });
-    gui.add(config, 'DYE_RESOLUTION', { '4k': 4096,'high': 1024, 'medium': 512, 'low': 256, 'very low': 128 }).name('quality').onFinishChange(initFramebuffers);
-    gui.add(config, 'SIM_RESOLUTION', { '32': 32, '64': 64, '128': 128, '256': 256,'512': 512,'1024': 1024 }).name('sim resolution').onFinishChange(initFramebuffers);
-    gui.add(config, 'DENSITY_DISSIPATION', 0, 4.0).name('density diffusion');
-    gui.add(config, 'VELOCITY_DISSIPATION', 0, 4.0).name('velocity diffusion');
+    gui.add(config, 'DYE_RESOLUTION', { '4k': 4096,'ultra high': 2048,'high': 1024, 'medium': 512, 'low': 256, 'very low': 128 }).name('quality').onFinishChange(initFramebuffers);
+    gui.add(config, 'SIM_RESOLUTION', { '32': 32, '64': 64, '128': 128, '256': 256,'512': 512,'1024': 1024,'2048': 2048,'4096': 4096 }).name('sim resolution').onFinishChange(initFramebuffers);
+    gui.add(config, 'DENSITY_DISSIPATION', 0, 4000.0).name('density diffusion');
+    gui.add(config, 'VELOCITY_DISSIPATION', 0, 4000.0).name('velocity diffusion');
     gui.add(config, 'PRESSURE', 0.0, 1.0).name('pressure');
     gui.add(config, 'CURL', 0, 50).name('vorticity').step(1);
     gui.add(config, 'SPLAT_RADIUS', 0.01, 1.0).name('splat radius');
@@ -224,12 +224,12 @@ function supportRenderTextureFormat (gl, internalFormat, format, type) {
 
     let bloomFolder = gui.addFolder('Bloom');
     bloomFolder.add(config, 'BLOOM').name('enabled').onFinishChange(updateKeywords);
-    bloomFolder.add(config, 'BLOOM_INTENSITY', 0.1, 2.0).name('intensity');
-    bloomFolder.add(config, 'BLOOM_THRESHOLD', 0.0, 1.0).name('threshold');
+    bloomFolder.add(config, 'BLOOM_INTENSITY', 0.1, 20.0).name('intensity');
+    bloomFolder.add(config, 'BLOOM_THRESHOLD', 0.0, 10.0).name('threshold');
 
     let sunraysFolder = gui.addFolder('Sunrays');
     sunraysFolder.add(config, 'SUNRAYS').name('enabled').onFinishChange(updateKeywords);
-    sunraysFolder.add(config, 'SUNRAYS_WEIGHT', 0.3, 1.0).name('weight');
+    sunraysFolder.add(config, 'SUNRAYS_WEIGHT', 0.3, 10.0).name('weight');
 
     let captureFolder = gui.addFolder('Capture');
     captureFolder.addColor(config, 'BACK_COLOR').name('background color');
